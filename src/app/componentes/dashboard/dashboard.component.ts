6842df0402b8ee4beb32bc39a156206c1bd4715a
Perez <ljperez@bpba.com.ar>
2020-10-27 15:00:04 -0300

    grafico

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InformeService } from 'src/app/servicios/informe.service';
 import { StorageService } from 'src/app/servicios/storage.service';
 
 @Component({
@@ -12,11 +13,27 @@ export class DashboardComponent implements OnInit {
   informes=false;
   flecha:string="abajo";
   flechamsn:string="abajo";
-  constructor(private _storage : StorageService ) { }
+  burzaco:number=0;
+  guernica:number=0;
+  long:number=0;
+  constructor(private _storage : StorageService, private _info : InformeService ) { }
 
   ngOnInit() {
     this.usuario=this._storage.getLocalStorage();
-    console.log(this.usuario);
+    
+    this._info.getInformesSucursal('Burzaco').subscribe((resp:any[])=>{
+      console.log("burzaco-->", resp)
+      this.burzaco= resp.length;
+    });
+    this._info.getInformesSucursal('Guernica').subscribe((resp:any[])=>{
+      console.log("guernica-->", resp)
+      this.guernica= resp.length;
+    });
+    this._info.getInformesSucursal('Longchamps').subscribe((resp:any[])=>{
+      console.log("long-->", resp)
+      this.long= resp.length;
+    });
+    
   }
 
   mensajes(){
