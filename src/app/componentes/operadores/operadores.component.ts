a0521189f5f3b3c583666e11a2e5cd7c878e9474
Perez <ljperez@bpba.com.ar>
2020-12-23 20:14:17 -0300

    Update operadores.component.ts

--- src/app/componentes/operadores/operadores.component.ts
+++ src/app/componentes/operadores/operadores.component.ts
@@ -17,23 +17,42 @@ export class OperadoresComponent implements OnInit {
   colores=["primary","secondary","success", "danger","Warning","Info", "Dark"];
   aux:string = this.colores[Math.floor(Math.random()*this.colores.length)]; 
   class = "badge badge-pill badge-"+this.aux;
-  guernica:any[]=[];
-  burzaco:any[]=[];
+  guernica1:any[]=[];
+  guernica2:any[]=[];
+  guernica3:any[]=[];
+  mercado:any[]=[];
+  guillon:any[]=[];
   longchamps:any[]=[];
   constructor(private _r: Router, private _usu: UsuarioService, private _repo : ReportesService ) { }
 
   ngOnInit() {
-    this._usu.getUsuariosPorSucursalOrdenados('Burzaco').subscribe((resp:any)=>{
-      this.burzaco=[];
+    this._usu.getUsuariosPorSucursalOrdenados('Guillon').subscribe((resp:any)=>{
+      this.guillon=[];
       for(let u of resp){
-        this.burzaco.push(u);
+        this.guillon.push(u);
       }
       
     });
-    this._usu.getUsuariosPorSucursalOrdenados('Guernica').subscribe((resp:any)=>{
-      this.guernica=[];
+    this._usu.getUsuariosPorSucursalOrdenados('Guernica1').subscribe((resp:any)=>{
+      this.guernica1=[];
       for(let u of resp){
-        this.guernica.push(u);
+        this.guernica1.push(u);
+      }
+      
+    });
+
+    this._usu.getUsuariosPorSucursalOrdenados('Guernica2').subscribe((resp:any)=>{
+      this.guernica2=[];
+      for(let u of resp){
+        this.guernica2.push(u);
+      }
+      
+    });
+
+    this._usu.getUsuariosPorSucursalOrdenados('Guernica3').subscribe((resp:any)=>{
+      this.guernica3=[];
+      for(let u of resp){
+        this.guernica3.push(u);
       }
       
     });
@@ -44,7 +63,17 @@ export class OperadoresComponent implements OnInit {
       }
       
     });
+
+    this._usu.getUsuariosPorSucursalOrdenados('Mercado central').subscribe((resp:any)=>{
+      this.mercado=[];
+      for(let u of resp){
+        this.mercado.push(u);
+      }
+      
+    });
   }
+
+
   
   reportes(item:any){
     this._repo.setOperador(item);
