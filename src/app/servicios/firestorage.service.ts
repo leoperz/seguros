650101c54367eb13e0401fec64e5575ba76811d3
Perez <ljperez@bpba.com.ar>
2021-04-18 22:05:27 -0300

    Update firestorage.service.ts

--- src/app/servicios/firestorage.service.ts
+++ src/app/servicios/firestorage.service.ts
@@ -9,8 +9,8 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class FirestorageService {
  
 
-  //urlBack:string = 'https://segurosalvarenga.herokuapp.com/';
-  urlBack:string = 'http://localhost:5500/';
+  urlBack:string = 'https://segurosalvarenga.herokuapp.com/';
+  //urlBack:string = 'http://localhost:5500/';
   constructor(private _fireStorage: AngularFireStorage,private _http: HttpClient) { }
 
 
